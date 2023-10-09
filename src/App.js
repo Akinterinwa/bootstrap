38923cdb067b5273f8767d9886da8ac2aa322305
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2023-10-09 14:40:14 +0100

    bootstrap conatiner created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Container, Nav, Card, Button } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default App;
+const Cards = () => {
+    return (
+        <>
+            <div className="App">
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand>My React App</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+
+                <Container>
+                    <h1>Welcome to My React App</h1>
+
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <Card>
+                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                                <Card.Body>
+                                    <Card.Title>Card 1</Card.Title>
+                                    <Card.Text>This is the first card content.</Card.Text>
+                                    <Button variant="primary">Learn More</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-sm-4">
+                            <Card>
+                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                                <Card.Body>
+                                    <Card.Title>Card 2</Card.Title>
+                                    <Card.Text>This is the second card content.</Card.Text>
+                                    <Button variant="primary">Learn More</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-sm-4">
+                            <Card>
+                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                                <Card.Body>
+                                    <Card.Title>Card 3</Card.Title>
+                                    <Card.Text>This is the third card content.</Card.Text>
+                                    <Button variant="primary">Learn More</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </>
+    );
+};
+
+export default Cards;
