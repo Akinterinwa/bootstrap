b4b2407b5be49d3b2767ffa0834ed5b4465cec0f
Akinterinwa Oluwanifemi <118985490+Akinterinwa@users.noreply.github.com>
2023-10-10 18:30:43 +0100

    created a card using bootstrap card and imported each component into the app.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,37 @@
+// src/App.js
+
 import React from "react";
-import { Navbar, Container, Nav, Card, Button } from "react-bootstrap";
-import "./App.css";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import Name from "./Name";
+import Price from "./Price";
+import Description from "./Description";
+import Image from "./Image";
+// import productData from './Product';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+export const firstName = "Oluwanifemi"; // Change this to your actual first name
 
-const Cards = () => {
+const App = () => {
+
     return (
-        <>
-            <div className="App">
-                <Navbar bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand>My React App</Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
+        <Container style={{ margin: '0 auto'}} className="display-grid text-center justify-content-center">
+            <Card style={{ width: '30rem', margin: '0 auto', backgroundColor: '#f0f0f0' }}>
+                <Card.Body  className="d-grid text-center justify-content-center">
+                <Image />
+                    <Card.Title><Name /></Card.Title>
+                    <Price />
+                    <Card.Text>
+                        <Description />
+                    </Card.Text>
 
-                <Container>
-                    <h1>Welcome to My React App</h1>
-
-                    <div className="row">
-                        <div className="col-sm-4">
-                            <Card>
-                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
-                                <Card.Body>
-                                    <Card.Title>Card 1</Card.Title>
-                                    <Card.Text>This is the first card content.</Card.Text>
-                                    <Button variant="primary">Learn More</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div className="col-sm-4">
-                            <Card>
-                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
-                                <Card.Body>
-                                    <Card.Title>Card 2</Card.Title>
-                                    <Card.Text>This is the second card content.</Card.Text>
-                                    <Button variant="primary">Learn More</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div className="col-sm-4">
-                            <Card>
-                                <Card.Img variant="top" src="https://via.placeholder.com/150" />
-                                <Card.Body>
-                                    <Card.Title>Card 3</Card.Title>
-                                    <Card.Text>This is the third card content.</Card.Text>
-                                    <Button variant="primary">Learn More</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    </div>
-                </Container>
+                </Card.Body>
+                <a style={{ width: '14rem', margin: '0 auto' }} className="btn btn-primary">Learn More</a>
+            </Card>
+            <div className="mt-4">
+                <h3>Hello, {firstName ? firstName : "there"}!</h3>
             </div>
-        </>
+        </Container>
     );
 };
 
-export default Cards;
+export default App;
